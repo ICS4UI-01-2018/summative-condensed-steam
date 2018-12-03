8a20a5ecfc5d3c87d4c0fb49cebd85e85d9cb3dc
Kalsr8025 <Kalsr8025@HRH-IC0027341.wrdsb.ca>
2018-12-03 08:47:00 -0500

    Enemies Class

---
+++ core/src/com/condensedsteam/EnemyShootPlayer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.condensedsteam;
+
+/**
+ *
+ * @author Kalsr8025
+ */
+public class EnemyShootPlayer extends Enemies{
+    
+}
