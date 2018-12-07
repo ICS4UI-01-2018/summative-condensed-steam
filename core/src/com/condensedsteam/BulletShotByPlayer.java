5f5e39f7424e3d56b1ec3b41fd2c3a61c60f7069
Kalsr8025 <Kalsr8025@HRH-IC0027341.wrdsb.ca>
2018-12-07 08:51:01 -0500

    working on bullet

--- core/src/com/condensedsteam/BulletShotByPlayer.java
+++ core/src/com/condensedsteam/BulletShotByPlayer.java
@@ -9,6 +9,13 @@ package com.condensedsteam;
  *
  * @author Kalsr8025
  */
-public class BulletShotByPlayer extends Player{
-    
+public class BulletShotByPlayer extends Player {
+    //Create a Bullet class to store the properties of your bullet. (e.g. speed, position)
+
+// your player class, create an array of your Bullet objects.
+//Set up event listener for when your user hits the 'shoot' button. 
+    //When they do add a bullet to your array. Starting position will be next
+    //to that of your player's.
+//When the bullet makes contact, or leaves the screen, remove it from your array. 
+    //(careful with this part ;)).
 }
