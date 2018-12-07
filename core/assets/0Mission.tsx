79b0f459b19906082c915cb4804833df1093bd6a
goodm9679 <goodm9679@HRH-IC0027342.wrdsb.ca>
2018-12-07 08:31:04 -0500

    oops

---
+++ core/assets/0Mission.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="0Mission" tilewidth="16" tileheight="16" tilecount="5120" columns="80">
+ <image source="zero mission.png" width="1280" height="1024"/>
+</tileset>
---
+++ core/assets/Fusion.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Fusion" tilewidth="16" tileheight="16" tilecount="2400" columns="48">
+ <image source="Fusion.png" width="768" height="800"/>
+</tileset>
