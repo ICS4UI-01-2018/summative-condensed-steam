4195d862d62cf5f2af11b380f92c050e1f44cfe2
Kalsr8025 <Kalsr8025@HRH-IC0027341.wrdsb.ca>
2019-01-08 08:14:43 -0500

    enemy moves with player

--- core/src/com/condensedsteam/Bullet.java
+++ core/src/com/condensedsteam/Bullet.java
@@ -12,66 +12,6 @@ import com.badlogic.gdx.math.Rectangle;
  *
  * @author Kalsr8025
  */
-//public class BulletShotByPlayer {
-//
-//    //Create a Bullet class to store the properties of your bullet. (e.g. speed, position)
-//    private float x;
-//    private float y;
-//    private float width;
-//    private float height;
-//    private int speed = 3;
-//    private Rectangle bullet;
-//    private Player player;
-//
-//    public void BulletShotByPlayer(float x, float y, float width, float height, int speed) {
-//        this.x = x;
-//        this.y = y;
-//        this.width = width;
-//        this.height = height;
-//        this.speed = speed;
-//        bullet = new Rectangle(x, y, 5, 5);
-//    }
-//
-//    public Rectangle getBounds() {
-//        return bullet;
-//    }
-//
-//    public boolean collidesWithEnemy(Enemy enemy, Fixed fixed) {
-//        if (bullet.overlaps(enemy.getBounds())) {
-//            return true;
-//        } else if (bullet.overlaps(fixed.getBounds())) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
-//
-//    public void move() {
-//        bullet.x = bullet.x + speed * x;
-//        bullet.y = bullet.y + speed * y;
-//    }
-//
-//    public float getLeft() {
-//        return bullet.x;
-//    }
-//
-//    public float getBottom() {
-//        return bullet.y;
-//    }
-//
-//    public float getRight() {
-//        return bullet.x + bullet.width;
-//    }
-//
-//    public float getTop() {
-//        return bullet.y + bullet.height;
-//    }
-//
-//    public void draw(ShapeRenderer shapeBatch) {
-//        shapeBatch.rect(bullet.x, bullet.y, bullet.width, bullet.height);
-//    }
-//}
-
 public class Bullet {
 
     private Rectangle bullet;
